d3160d28b9effef06686875a8d16a436e9301f11
Jeel Shah <jeelshah96@gmail.com>
2023-06-03 18:07:32 -0400

    NO-ISSUE: Adds. a basic array of training data

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+float train[][2] = {
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8},
+};
+
+float rand_float(void) {
+    return (float) rand() / (float) RAND_MAX;
+}
 
 int main() {
-    printf("Hello World! \n");
 
+    // We do this so that our random number is different each time. We seed it with the current time.
+    srand(time(0));
+
+    float w = rand_float() * 10.0f;
+
+    printf("%f \n", w);
     return 0;
 }
